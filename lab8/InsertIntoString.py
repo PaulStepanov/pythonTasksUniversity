2f05d724083e2ebd11827bdc60e34509a16ab8b6
zarabon <freestalker1987@gmail.com>
2016-09-29 23:37:08 +0300

    lab 8 full done,also i've wrote a function which inserts string into string after assigned index

--- lab8/InsertIntoString.py
+++ lab8/InsertIntoString.py
@@ -1,7 +1,7 @@
-# insert insetring after index of string
+# insert insertString into string  after assigned index
 
-def insertIntoString(string,insetring,index):
+def insertIntoString(string, insertString, index):
     str1 = string[0:index + 1]
     str2 = string[index + 1:len(string)]
     que = (str1, str2)
-    return insetring.join(que)
+    return insertString.join(que)
