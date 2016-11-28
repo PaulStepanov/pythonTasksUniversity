d0adb3054c074bbdffbb35318bf1126c37786702
Павел <freestalker1987@gmail.com>
2016-11-28 17:06:55 +0300

    8.3and 9.1 done

--- lab83/lab83.py
+++ lab83/lab83.py
@@ -1,16 +1,13 @@
-L=['a',1,-2,1.2]
-print('1:',[x for x in L if type(x)==int and x%2==0])
-print('2: ',[L[i] for i in range(len(L)) if i%2==0])
-print('3: ',[x for x in L if type(x)!=str and x>0 ])
-L=[1,3,9,7,4,11,14]
-m=3
-print('4: ',[{x for x in L if x%m==y} for y in {x%m for x in L} ])
-L={1:2,'a':3,-1:1,3:'a'}
-print('5: ',[x+L[x] for x in L if type(x)!=str and type(L[x])!=str])
-L=[3,4.1,2]
-print('6: ',[(x,y) for x in L for y in L if x<y])
-L=['a',1,-2,1.2]
-print('7: ',{L[i]:L[i+1] for i in range(len(L)-1) if i%2==0})
-
-
-
+L = ['a', 1, -2, 1.2]
+print('1:', [x for x in L if type(x) == int and x % 2 == 0])
+print('2: ', [L[i] for i in range(len(L)) if i % 2 == 0])
+print('3: ', [x for x in L if type(x) != str and x > 0])
+L = [1, 3, 9, 7, 4, 11, 14]
+m = 3
+print('4: ', [{x for x in L if x % m == y} for y in {x % m for x in L}])
+L = {1: 2, 'a': 3, -1: 1, 3: 'a'}
+print('5: ', [x + L[x] for x in L if type(x) != str and type(L[x]) != str])
+L = [3, 4.1, 2]
+print('6: ', [(x, y) for x in L for y in L if x < y])
+L = ['a', 1, -2, 1.2]
+print('7: ', {L[i]: L[i + 1] for i in range(len(L) - 1) if i % 2 == 0})
